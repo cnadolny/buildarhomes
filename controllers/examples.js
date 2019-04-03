5ac801e9ba93c516d6cbe64341f1a1df1b329601
Claudia Nadolny <nadolny.claudia@gmail.com>
2019-04-02 20:48:42 -0700

    added more pictures

--- controllers/examples.js
+++ controllers/examples.js
@@ -5,12 +5,15 @@ exports.index = (req, res) => {
 		fs.readdir('public/images/golfview', function(err, golfviewData){
 			fs.readdir('public/images/tuscany', function(err, tuscanyData){
 				fs.readdir('public/images/fairhope', function(err, fairhopeData){
-					res.render('examples', {
-						title: 'Examples',
-						linden: lindenData,
-						golfview: golfviewData,
-						tuscany: tuscanyData,
-						fairhope: fairhopeData
+					fs.readdir('public/images/queens', function(err, queensData){
+						res.render('examples', {
+							title: 'Examples',
+							linden: lindenData,
+							golfview: golfviewData,
+							tuscany: tuscanyData,
+							fairhope: fairhopeData,
+							queens: queensData
+						});
 					});
 				});
 			});
