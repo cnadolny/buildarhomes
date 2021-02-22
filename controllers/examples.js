74daefe90953f64f4be82dd1c45eec2ef5242872
Claudia Nadolny <nadolny.claudia@gmail.com>
2021-02-21 20:46:54 -0800

    added new images

--- controllers/examples.js
+++ controllers/examples.js
@@ -6,13 +6,16 @@ exports.index = (req, res) => {
 			fs.readdir('public/images/tuscany', function(err, tuscanyData){
 				fs.readdir('public/images/fairhope', function(err, fairhopeData){
 					fs.readdir('public/images/queens', function(err, queensData){
-						res.render('examples', {
-							title: 'Examples',
-							linden: lindenData,
-							golfview: golfviewData,
-							tuscany: tuscanyData,
-							fairhope: fairhopeData,
-							queens: queensData
+						fs.readdir('public/images/glenoak', function(err, glenoakData){
+							res.render('examples', {
+								title: 'Examples',
+								linden: lindenData,
+								golfview: golfviewData,
+								tuscany: tuscanyData,
+								fairhope: fairhopeData,
+								queens: queensData,
+								glenoak: glenoakData
+							});
 						});
 					});
 				});
